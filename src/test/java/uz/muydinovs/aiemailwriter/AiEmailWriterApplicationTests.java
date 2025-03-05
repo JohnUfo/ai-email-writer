0f1489f1966aee25b310273eb65189f0742937b1
JohnUfo <tursunov.umidjon.uz@gmail.com>
2025-03-05 23:56:16 +0500

    first commit

---
+++ src/test/java/uz/muydinovs/aiemailwriter/AiEmailWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.muydinovs.aiemailwriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEmailWriterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
